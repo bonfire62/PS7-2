318a2ae179120f9e7746018935d30f5124da39d2
Ken Bonar <kenbonar@gmail.com>
2016-10-26 21:13:02 -0600

    stack working, reading in the input to the stack, need to implement the grammar stack

--- PS7-2/Program.cs
+++ PS7-2/Program.cs
@@ -1,59 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PS7_2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            StreamReader reader = new StreamReader(args[0]);
-            char[] nonterminals = reader.ReadLine().ToCharArray();
-            char[] terminals = reader.ReadLine().ToCharArray();
-            int grammmar_length = Convert.ToInt32(reader.ReadLine());
-            char[,] grammar = new char[nonterminals.Length, terminals.Length+2];
-            string[] mapped_terminals = new string[grammmar_length+1];
+            var langText = new StreamReader(args[0]);
+            var inpText = new StreamReader(args[1]);
+            var nonterminals = langText.ReadLine().ToCharArray();
+            var terminals = langText.ReadLine().ToCharArray();
+            var grammmar_length = Convert.ToInt32(langText.ReadLine());
+            var grammar = new char[nonterminals.Length, terminals.Length + 2];
+            var mapped_terminals = new string[grammmar_length + 1];
+
             
 
             // create 2d array for parse table
             // populate columns with the nonterminals
-            for (int i = 0; i < nonterminals.Length; i++)
+            for (var i = 0; i < nonterminals.Length; i++)
             {
-                grammar[i,0] = nonterminals[i];
+                grammar[i, 0] = nonterminals[i];
             }
 
-            
 
             //  parse and map terminals to digits
-            for (int i = 1; i < grammmar_length+1; i++)
+            for (var i = 1; i < grammmar_length + 1; i++)
             {
-                string[] testchar = (reader.ReadLine().Split());
+                var testchar = langText.ReadLine().Split();
                 mapped_terminals[i] = testchar[1];
-                Console.WriteLine(mapped_terminals[i]);
-
+                //Console.WriteLine(mapped_terminals[i]);
             }
 
-            // parse table values in
-            for (int i = 0; i < nonterminals.Length; i++)
+            // parse in table values 
+            for (var i = 0; i < nonterminals.Length; i++)
             {
-                string[] table_ints = reader.ReadLine().Split();
+                var table_ints = langText.ReadLine().Split();
 
-                for (int j = 0; j < terminals.Length+1; j++)
+                for (var j = 0; j < terminals.Length + 1; j++)
                 {
-                    
-                    int int_conv = Convert.ToInt32(table_ints[j]);
-                    grammar[i,j+1] = Convert.ToChar(int_conv);
+                    var int_conv = Convert.ToInt32(table_ints[j]);
+                    grammar[i, j + 1] = Convert.ToChar(int_conv);
                 }
-                
-                    
-
-                    
-                
             }
+
+            while (!inpText.EndOfStream)
+            {
+                var charStack = new Stack();
+                var stringStack = new Stack();
+
+                string inpString = inpText.ReadLine();
+                char[] charstring = inpString.ToCharArray();
+                foreach (var letter in charstring)
+                {
+                    stringStack.Push(letter);
+                }
+            }
+
 
 
             Console.ReadLine();
@@ -65,8 +70,6 @@ namespace PS7_2
             //                Console.WriteLine(nonterminals.Length);
             //                Console.ReadLine();
             //            }
-
-
         }
     }
 }
